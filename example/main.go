a4626e3487adeda1f074a2080bedb274d01a313f
Melissa Geels <spansjh@gmail.com>
2022-03-26 13:58:21 +0100

    Updated example with local testing http server

--- example/main.go
+++ example/main.go
@@ -2,22 +2,43 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"net/http"
 	"time"
 
 	rollingcache "github.com/codecat/go-rollingcache"
 )
 
+func testserver() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Duration(rand.Int()%5) * time.Second)
+		if rand.Int()%10 < 7 {
+			w.WriteHeader(500)
+			w.Write([]byte("Internal server error"))
+			return
+		}
+		w.WriteHeader(200)
+		w.Write([]byte("It works: " + time.Now().Format(time.RFC3339)))
+	})
+	http.ListenAndServe(":8900", nil)
+}
+
 func main() {
+	go testserver()
+
 	rollingcache.HttpHeaders["User-Agent"] = "github.com/codecat/go-rollingcache test"
 
-	cache := rollingcache.Start("https://httpbin.org/get", rollingcache.Options{
-		Interval: 10 * time.Second,
-		Debug:    true,
+	cache := rollingcache.Start("http://127.0.0.1:8900/", rollingcache.Options{
+		Interval:     10 * time.Second,
+		FailInterval: 3 * time.Second,
+		MaxRetries:   3,
+		Debug:        true,
 	})
 
 	for i := 0; i < 100; i++ {
+		fmt.Printf("Request %d ... ", i+1)
 		res := cache.Get()
-		fmt.Printf("Request %d data length: %d bytes\n", i+1, len(res))
+		fmt.Printf("got: %s\n", string(res))
 		time.Sleep(1337 * time.Millisecond)
 	}
 
